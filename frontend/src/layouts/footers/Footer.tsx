4f86bee316a7bdc3957b0edae15c6d5af08b0970
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2025-08-14 15:55:41 +0900

    style : 메뉴바 아이콘 변경
    author : 김민정
     - 아이콘 변경
     - 색상 변경 & 호버 기능

--- frontend/src/layouts/footers/Footer.tsx
+++ frontend/src/layouts/footers/Footer.tsx
@@ -8,30 +8,30 @@ const Footer = () => {
 			<footer className="bottom-nav">
 				<ul className="d-flex align-items-center justify-content-around w-100 h-100">
 					<li>
-						<Link to="/home">
+						<Link to="/home" className="icon home">
 							<img src="/assets/svg/bottom-nav/home.svg" alt="home" />
 						</Link>
 					</li>
 					<li>
-						<Link to="/explore">
-							<img src="/assets/svg/bottom-nav/category.svg" alt="category" />
+						<Link to="/explore" className="icon edit">
+							<img src="/assets/svg/edit.svg" alt="todolist" />
 						</Link>
 					</li>
 					<li>
-						<Link to="/ticket-booked">
-							<img src="/assets/svg/bottom-nav/ticket.svg" alt="ticket" />
+						<Link to="/explore" className="icon calendar">
+							<img src="/assets/svg/calendar.svg" alt="calendar" />
 						</Link>
 					</li>
 					<li>
-						<Link to="/wishlist">
-							<img src="/assets/svg/bottom-nav/heart.svg" alt="heart" />
+						<Link to="/wishlist" className="icon map">
+							<img src="/assets/svg/map-marker.svg" alt="map" />
 						</Link>
 					</li>
 					<li>
-						<Link to="/user-profile">
+						<Link to="/user-profile" className="icon message">
 							<img
-								src="/assets/svg/bottom-nav/profile-active.svg"
-								alt="profile"
+								src="/assets/svg/message-square-dots.svg"
+								alt="chat"
 							/>
 						</Link>
 					</li>
