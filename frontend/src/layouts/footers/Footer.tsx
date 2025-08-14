1e25e2a3a67e7c725b59f4bea6e914657642ae40
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2025-08-14 17:04:43 +0900

    change : 메뉴 아이콘 중복 제거
    author : 김민정
     - <a>,<img>태그로 아이콘 중복 현상 <img> 태그 제거

--- frontend/src/layouts/footers/Footer.tsx
+++ frontend/src/layouts/footers/Footer.tsx
@@ -8,32 +8,19 @@ const Footer = () => {
 			<footer className="bottom-nav">
 				<ul className="d-flex align-items-center justify-content-around w-100 h-100">
 					<li>
-						<Link to="/home" className="icon home">
-							<img src="/assets/svg/bottom-nav/home.svg" alt="home" />
-						</Link>
+						<Link to="/home" className="icon home"></Link>
 					</li>
 					<li>
-						<Link to="/explore" className="icon edit">
-							<img src="/assets/svg/edit.svg" alt="todolist" />
-						</Link>
+						<Link to="/explore" className="icon edit"></Link>
 					</li>
 					<li>
-						<Link to="/explore" className="icon calendar">
-							<img src="/assets/svg/calendar.svg" alt="calendar" />
-						</Link>
+						<Link to="/explore" className="icon calendar"></Link>
 					</li>
 					<li>
-						<Link to="/wishlist" className="icon map">
-							<img src="/assets/svg/map-marker.svg" alt="map" />
-						</Link>
+						<Link to="/wishlist" className="icon map"></Link>
 					</li>
 					<li>
-						<Link to="/user-profile" className="icon message">
-							<img
-								src="/assets/svg/message-square-dots.svg"
-								alt="chat"
-							/>
-						</Link>
+						<Link to="/user-profile" className="icon message"></Link>
 					</li>
 				</ul>
 			</footer>
