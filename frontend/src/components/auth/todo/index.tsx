6f5b3ab4c6718c2028b7d69d62eaa8fc03342fa2
pssu9412 <psk1221@daum.net>
2025-08-19 15:23:10 +0900

    Create: todo 생성
    author: 박수경
    - todo 폴더 및 index.tsx 생성

---
+++ frontend/src/components/auth/todo/index.tsx
@@ -0,0 +1,17 @@
+
+import HeaderOne from "../../../layouts/headers/HeaderOne";
+import Footer from "../../../layouts/footers/Footer";
+
+
+const TodoMain = ()=> {
+    return (
+      <>
+        <main className="home">
+          <HeaderOne />
+        </main>
+        <Footer />
+      </>
+    );
+};
+
+export default TodoMain;
