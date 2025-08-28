872a7f6b749550c3d50e7f0a368e5558a63351fe
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2025-08-28 16:14:16 +0900

    Change & style : 캘린더 구성 및 스타일 수정
    author: 김민정

--- frontend/src/components/auth/calendar/index.tsx
+++ frontend/src/components/auth/calendar/index.tsx
@@ -8,17 +8,20 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+import koLocale from '@fullcalendar/core/locales/ko';
 
 // calendar CSS
 import '@/assets/css/calendar.css';
 
 export default function Calendar(){
-
+    
+    /*
     useEffect(() => {
         // Calendar 페이지 진입 시에만 CSS import
         import('bootstrap/dist/css/bootstrap.min.css');
         import("bootswatch/dist/sketchy/bootstrap.min.css");
     }, []);
+    */
 
     const plugins = [dayGridPlugin, timeGridPlugin, interactionPlugin, bootstrap5Plugin];
 
@@ -32,9 +35,10 @@ export default function Calendar(){
                         plugins={plugins}
                         initialView="dayGridMonth"
                         themeSystem="bootstrap5"
+                        locale={koLocale}
                         headerToolbar={{
-                            left:'prev,today,next',  
-                            center:'title',
+                            left:'today',  
+                            center:'prev title next',
                             right:'dayGridMonth,timeGridWeek,timeGridDay' 
                         }}
                         buttonText={{
@@ -43,11 +47,18 @@ export default function Calendar(){
                             day:'일',
                             prev:'<',
                             next:'>',
-                            today:'ㅇ'
+                            today:'오늘'
                         }}
                         height="auto"
                         contentHeight="auto"
-                        firstDay={1}
+                        firstDay={0}
+                        dayCellContent={arg => arg.date.getDate()}
+                        dayCellClassNames={arg => {
+                            const day = arg.date.getDay();
+                            if (day === 0) return 'fc-sunday'; // 일요일
+                            if (day === 6) return 'fc-saturday'; // 토요일
+                            return '';
+                        }}
                     />
                 </div>
             </main>
