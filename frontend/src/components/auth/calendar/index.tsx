079fa5eff61ebb31d8ffa84b771027e09da51c0c
dandi0624 <153481807+dandi0624@users.noreply.github.com>
2025-09-04 16:00:06 +0900

    Fix : 캘린더 이벤트 관련 코드 수정
    author : 김민정
     - 코드 미완성 주석 처리

--- frontend/src/components/auth/calendar/index.tsx
+++ frontend/src/components/auth/calendar/index.tsx
@@ -1,10 +1,11 @@
 //import { useEffect } from "react";
+import { useState } from "react";
 import HeaderOne from "../../../layouts/headers/HeaderOne";
 import Footer from "../../../layouts/footers/Footer";
 
 // FullCalendar
 import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
+import dayGridPlugin from '@fullcalendar/daygrid' 
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
@@ -23,8 +24,67 @@ export default function Calendar(){
     }, []);
     */
 
+    /* 일정 상태(State) 관리 
+    const [events, setEvents] = useState([]); // 전체 일정 목록
+    const [selectedDate, setSelectedDate] = useState(null); // 선택한 날짜
+    const [showListModal, setShowListModal] = useState(false); // 일정 목록 표시 여부
+    const [showAddModal, setShowAddModal] = useState(false); // 일정 추가 표시 여부
+    */  
+
+    /* 임시 데이터
+    const [newEvent, setNewEvent] = useState({
+        title: "",
+        start:"",
+        end:"",
+        color: "#ff4d4d",
+        description: ""
+    })
+    */
+
+    // 일정 색상
+    const colorOptions = [
+        { name : "pink", value: "#FFCCEA"},
+        { name : "yellow", value: "#FFF6E3"},
+        { name : "purple", value: "#CDC1FF"},
+        { name : "blue", value: "#BFECFF"}
+    ];
+
+
+    // 풀캘린더 플러그인 목록
     const plugins = [dayGridPlugin, timeGridPlugin, interactionPlugin, bootstrap5Plugin];
 
+
+    // ===================== 이벤트 핸들러 함수 ==============================
+/*
+    // 날짜 클릭시 실행
+    const handleDateClick = (arg) => {
+        setSelectedDate(arg.dateStr); // -> yyyy-mm-dd
+        setShowListModal(true); // 일정 목록 모달
+    };
+
+    // 일정 추가 실행
+    const handleAddclick = () => {
+        // 새로운 일정 데이터의 시작일과 종료일을 클릭한 날짜로 초기화
+        setNewEvent({
+            title: "",
+            start: selectDate,
+            end: selectDate,
+            color: "#FFCCEA",
+            description: "",
+        });
+        // 일정 추가 모달 띄우기
+        setShowAddModal(true);
+    };
+
+    // 일정 등록 폼 제출 시 실행
+    const handleAddEvent = (e) => {
+        e.preventDefault(); // 페이지 새로고침 방지
+        setEvents([...EventSource, newEvent]); // 현재 일정 목록에 새로운 일정 추가
+        setShowAddModal(false);
+        setShowListModal(false);
+    }
+ */
+/*
     return (
         <>
             <main>
@@ -33,7 +93,7 @@ export default function Calendar(){
                 <div className="container my-3 main-calendar-wrapper calendar-page">
                     <FullCalendar
                         plugins={plugins}
-                        initialView="dayGridMonth"
+                        initialView="sqdayGridMonth"
                         themeSystem="bootstrap5"
                         locale={koLocale}
                         headerToolbar={{
@@ -51,6 +111,8 @@ export default function Calendar(){
                         }}
                         editable={true}
                         selectable={true}
+                        events={events}
+                        dateClick={handleDateclick}
                         eventStartEditable={true}
                         eventDurationEditable={true}
                         height="auto"
@@ -65,6 +127,38 @@ export default function Calendar(){
                         }}
                     />
                 </div>
+*/
+                {/* 일정 목록 모달 */}
+               // {showListModal && (
+                    <div className="-modal-dialog">
+                        <h3> 일정 목록</h3>
+                        <div className="modal-content">
+                            {/* 선택된 날짜에 해당하는 일정만 필터링*/}
+                           
+                        </div>
+                    </div>
+                //)}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             </main>
             <Footer />    
         </>
