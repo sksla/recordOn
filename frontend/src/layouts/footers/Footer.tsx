7f741e0cf68eccc86e1b3660b560a2fc1141b8bc
sksla <153481660+sksla@users.noreply.github.com>
2025-09-04 14:06:53 +0900

    Update : Footer 메세지 아이콘에 링크 연결
    author : 김영주
    
    Footer 안 메세지 아이콘에 채팅방 목록으로 이동하는 링크로 연결

--- frontend/src/layouts/footers/Footer.tsx
+++ frontend/src/layouts/footers/Footer.tsx
@@ -1,31 +1,29 @@
 import { Link } from "react-router-dom";
 
- 
-
 const Footer = () => {
-	return (
-		<>
-			<footer className="bottom-nav">
-				<ul className="d-flex align-items-center justify-content-around w-100 h-100">
-					<li>
-						<Link to="/home" className="icon home"></Link>
-					</li>
-					<li>
-						<Link to="/todo" className="icon edit"></Link>
-					</li>
-					<li>
-						<Link to="/calendar" className="icon calendar"></Link>
-					</li>
-					<li>
-						<Link to="/map" className="icon map"></Link>
-					</li>
-					<li>
-						<Link to="/user-profile" className="icon message"></Link>
-					</li>
-				</ul>
-			</footer>
-		</>
-	);
+  return (
+    <>
+      <footer className="bottom-nav">
+        <ul className="d-flex align-items-center justify-content-around w-100 h-100">
+          <li>
+            <Link to="/home" className="icon home"></Link>
+          </li>
+          <li>
+            <Link to="/todo" className="icon edit"></Link>
+          </li>
+          <li>
+            <Link to="/calendar" className="icon calendar"></Link>
+          </li>
+          <li>
+            <Link to="/map" className="icon map"></Link>
+          </li>
+          <li>
+            <Link to="/message" className="icon message"></Link>
+          </li>
+        </ul>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
